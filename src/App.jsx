ac9271a15b4bcd829e766e0f472d1ba2ec61a623
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-09-09 19:06:31 +0300

    point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import user from './data/user';
-import data from './data/data';
-import friends from './data/friends';
-import transactions from './data/transactions';
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import user from 'data/user';
+import data from 'data/data';
+import friends from 'data/friends';
+import transactions from 'data/transactions';
 
 export const App = () => (
 <>
@@ -25,5 +25,5 @@ export const App = () => (
 
       <TransactionHistory items={transactions} />
       </>
-  )
+  );
 // export default App;
