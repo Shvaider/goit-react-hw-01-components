437cecdeedf6654596bd34232cdd16b9133029a6
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-09-09 18:46:39 +0300

    user

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import user from './data/user';
+import data from './data/data';
+import friends from './data/friends';
+import transactions from './data/transactions';
+
+export const App = () => (
+<>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <Statistics stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+      </>
+  )
+// export default App;
